8f02d908672e7b48260acc6d0af2b39f4c8880aa
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-07-11 18:44:51 -0300

    left logged in scene, cards hover -3px

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :suppres_flash
   allow_browser versions: :modern
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+  end
+  def suppres_flash
+    flash.clear
   end
 end
