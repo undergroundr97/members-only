b51603feda47dc26fdfb7e9f8230847f06428882
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-07-14 15:57:03 -0300

    making up right-side content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,7 +63,9 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  # def last_four
+  #   @posts.reverse.take(4)
+  # end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   resources :users
-  root "posts#new"
+  root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
