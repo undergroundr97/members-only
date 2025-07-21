44abecb42dd06809e2291089e99b5f053fac9840
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-07-21 17:22:11 -0300

    added font to whole website, fixing color pallete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :title, length: { minimum: 10 }, presence: true
   validates :title, presence: true
   belongs_to :user
 end
