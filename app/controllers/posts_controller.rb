9644d4a0be6ae4e37a063aa2837f6ac56d7fd3d2
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-07-26 20:35:05 -0300

    made a search bar, removed the img logo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    if params[:query].present?
+      @posts = Post.where("title LIKE ?", "%#{params[:query].downcase}%")
+    else
+      @posts = Post.all
+    end
   end
 
   # GET /posts/1 or /posts/1.json
@@ -69,7 +73,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params.expect(:id))
+        @post = Post.find(params.expect(:id))
     end
 
     # Only allow a list of trusted parameters through.
