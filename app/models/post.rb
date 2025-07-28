eb59a8376029bd7d8898c85282824bd3ad5560c0
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-07-28 15:58:40 -0300

    post show container

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, length: { minimum: 10 }, presence: true
-  validates :title, presence: true
+  validates :title, length: { minimum: 10, maximum: 70 }, presence: true
+  validates :title, length: { minimum: 0, maximum: 300 }, presence: true
   belongs_to :user
 end
