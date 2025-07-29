decfb22cfae9119bf20eac445bbb144a420389b6
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-07-29 18:54:42 -0300

    finished edit show post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :title, length: { minimum: 10, maximum: 70 }, presence: true
-  validates :title, length: { minimum: 0, maximum: 300 }, presence: true
+  validates :content, length: { minimum: 0, maximum: 300 }
   belongs_to :user
 end
